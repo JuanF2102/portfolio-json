b961caaf076d3705f194c863ec54ec1c653e3a09
Alejandro Font Muñiz <91348432+AleDev11@users.noreply.github.com>
2024-10-27 14:28:56 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://AleDev11.github.io',
+    site: 'https://cv.aledev.es',
     base: 'portfolio-json',
 });
