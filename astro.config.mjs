7e91fa53e66da592079f602ad8bfb37942c41fb0
JuanF2102 <jfontmuniz@gmail.com>
2024-11-11 14:09:07 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://juanf2102.github.io',
-    base: '',
+    base: 'portfolio-json',
 });
